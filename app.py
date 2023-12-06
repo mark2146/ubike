9a88c0c2aec1e69e7fb546c4eee90266297ae3dc
mark798 <mark1050373@gmail.com>
2023-12-06 23:27:58 +0800

    FLASK

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
 from flask import Flask, render_template
+from flask import jsonify
 
-app = Flask(__name__)
+import requests
+
+app = Flask(__name__, static_folder='immage')
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/rate')
+def rate():
+    return render_template('rate.html')
+
+@app.route('/self')
+def self_page():
+    return render_template('self.html')
+
+@app.route('/data')
+def get_data():
+    response = requests.get('https://datacenter.taichung.gov.tw/swagger/OpenData/86dfad5c-540c-4479-bb7d-d7439d34eeb1')
+    data = response.json()
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
